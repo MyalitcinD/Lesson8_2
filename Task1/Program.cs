a69e8599baed7408c6dc5ce05fc96ff1e5b6eba1
Денис <mden1996.07@gmail.com>
2024-03-07 01:49:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"D:\UNIK\les8_task1";
+            string[] folders = Directory.GetFiles(folderPath, "*",SearchOption.AllDirectories);
+            foreach(string s in folders) {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
